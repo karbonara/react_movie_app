404aad266396f20dc17aa965c48ab88674a022a8
Karbonara <37299335+karbonara@users.noreply.github.com>
2022-03-28 23:57:39 +0300

    Внес правки в верстку

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,10 @@ const Input = styled.input`
 width: 90vw;
 margin: 20px 0;
 padding: 8px;
-font-size: 24px
+font-size: 24px;
+background: #484848;
+color: #fff;
+border-radius: 3px;
 `;
 const Button = styled.button`
 width: 120px;
@@ -12,6 +15,11 @@ padding: 8px;
 font-size: 16px;
 background: #e5e5e5;
 border: none;
+background: #f60;
+border: none;
+color: #fff;
+font-weight: 900;
+border-radius: 3px;
 `;
 
 function Form({ searchMovies, setSearch }) {
